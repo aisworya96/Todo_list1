9e7510aba2e4a758c272a9005977dd412f5ec874
apple <aisworyapanda@gmail.com>
2025-05-07 10:39:01 +0530

    New file modified

--- Todo.py
+++ Todo.py
@@ -1,4 +1,20 @@
+import mysql.connector
 
+conn = mysql.connector.connect(
+    host="localhost",
+    user="todo_user",       # 👈 Your new username
+    password="todo_pass",   # 👈 Your new password
+    database="todo_app"     # 👈 Your database name
+)
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(255) NOT NULL
+)
+''')
+conn.commit()
 
 todo_list = []
 
@@ -16,8 +32,24 @@ def view_tasks():
     for task in todo_list:
         print(str(i) + "." + task )
         i = i + 1
+
+def update_tasks(index, new_task):
+    try:
+
+        todo_list[index] = new_task
+        print("New task updated.")
+    except IndexError:
+        print("Invalid Index Number.")
+
+
+
+
 add_tasks()
 view_tasks()
+update_tasks(0, "Updated Task")
 
+
+cursor.close()
+conn.close()
 
 
